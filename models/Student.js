4d78fa31ed4ac1b8fcf5f522f770aa516311412b
Swapnil <Swapnil.kasabe@afourtech.com>
2024-09-20 17:32:27 +0530

    feat: Add indexing to Mongoose schemas

--- models/Student.js
+++ models/Student.js
@@ -12,6 +12,8 @@ const studentSchema = new mongoose.Schema({
   isDeleted: { type: Boolean, default: false },
 });
 
+studentSchema.index({ studentId: 1, email: 1 });
+
 const Student = mongoose.model("Student", studentSchema);
 
 export default Student;
--- models/User.js
+++ models/User.js
@@ -10,6 +10,8 @@ const UserSchema = new mongoose.Schema({
   role: { type: String, enum: roles },
 });
 
+UserSchema.index({ email: 1 });
+
 export const UserModel = mongoose.model("User", UserSchema);
 
 export default UserModel;
